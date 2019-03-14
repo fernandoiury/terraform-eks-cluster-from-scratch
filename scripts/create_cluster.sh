7a528a9ad0602035b246d6dff05985ecb64b3d8c
Fernando Costa <fernando.costa@paytm.com>
2019-03-14 15:29:00 -0400

    Adds autoscaler / improves deploy script

--- scripts/create_cluster.sh
+++ scripts/create_cluster.sh
@@ -6,14 +6,14 @@ for i in ${REQUIRED_SW}; do
   which ${i} &> /dev/null
   if [[ ${?} -ne 0 ]]; then
     echo "${i} not found in your path"
-    exit 2
+    exit 1
   fi 
 done
 
 AWS_PROF="${1}"
 if [[ -z ${AWS_PROF} ]]; then
    echo "USAGE: $(basename $0) <AWS_PROFILE>"
-   exit 1
+   exit 2
 fi
 
 function print_log() {
@@ -23,6 +23,7 @@ if [[ ${1} -ne 0 ]]; then
   echo "###### NOK - $(date +"%Y-%m-%d %H:%M:%S") - ${2} - exitting"
   echo "######"
   echo
+  exit 3
 else
   echo
   echo "######"
@@ -47,21 +48,36 @@ print_log $? "Terraform Applied Sucessfully"
 
 # configuring kubectl
 [[ $(stat ~/.kube/config &> /dev/null) ]] && echo "Moving ~/.kube/config to ~/.kube/config.bak" && mv ~/.kube/config stat ~/.kube/config.bak
-CLUSTER_NAME=$(grep 'create_cluster.sh' ../vars.tf | awk '{ print $3 }' | sed 's/"//g')
-aws eks --region us-east-1 update-kubeconfig --name ${CLUSTER_NAME} --profile ${AWS_PROF}
+CLUSTER_NAME=$(grep 'create_cluster.sh name' ../vars.tf | awk '{ print $3 }' | sed 's/"//g')
+CLUSTER_REGION=$(grep 'create_cluster.sh region' ../vars.tf | awk '{ print $3 }' | sed 's/"//g')
+aws eks --region ${CLUSTER_REGION} update-kubeconfig --name ${CLUSTER_NAME} --profile ${AWS_PROF}
 print_log $? "~/.kube/config configured"
 
 # configuring ALB
-curl -s -O https://raw.githubusercontent.com/kubernetes-sigs/aws-alb-ingress-controller/v1.1.0/docs/examples/alb-ingress-controller.yaml
-curl -s -O https://raw.githubusercontent.com/kubernetes-sigs/aws-alb-ingress-controller/v1.1.0/docs/examples/rbac-role.yaml
-sed -i '' "s/devCluster/${CLUSTER_NAME}/g" alb-ingress-controller.yaml
-kubectl apply -f rbac-role.yaml
-kubectl apply -f alb-ingress-controller.yaml
+curl -s -O https://raw.githubusercontent.com/kubernetes-sigs/aws-alb-ingress-controller/v1.1.0/docs/examples/alb-ingress-controller.yaml && \
+curl -s -O https://raw.githubusercontent.com/kubernetes-sigs/aws-alb-ingress-controller/v1.1.0/docs/examples/rbac-role.yaml && \
+sed -i '' "s/devCluster/${CLUSTER_NAME}/g" alb-ingress-controller.yaml && \
+kubectl apply -f rbac-role.yaml && \
+kubectl apply -f alb-ingress-controller.yaml && \
+rm -f rbac-role.yaml alb-ingress-controller.yaml
 print_log $? "alb-ingress-controller"
+
+# Configuring cluster-autoscaler - for now fixing the version 1.3.6 which is the recommended for k8s 1.11.X (some users report master works, but let's not break things (: )
+curl -s -O https://raw.githubusercontent.com/kubernetes/autoscaler/cluster-autoscaler-1.3.6/cluster-autoscaler/cloudprovider/aws/examples/cluster-autoscaler-autodiscover.yaml && \
+sed -i '' "s:k8s.io/cluster-autoscaler/<YOUR CLUSTER NAME>:kubernetes.io/cluster/${CLUSTER_NAME}:g" cluster-autoscaler-autodiscover.yaml && \
+sed -i '' "s/us-east-1/${CLUSTER_REGION}/g" cluster-autoscaler-autodiscover.yaml && \
+sed -i '' '$s:/etc/ssl/certs/ca-certificates.crt:/etc/ssl/certs/ca-bundle.crt:' cluster-autoscaler-autodiscover.yaml && \
+kubectl apply -f cluster-autoscaler-autodiscover.yaml && \
+rm -f cluster-autoscaler-autodiscover.yaml
+print_log $? "cluster-autoscaler"
 
 # TODO add more stuff
 
+
+# remove temp files
+rm -f ../*${CLUSTER_NAME}*
 print_log $? "Kubernetes Cluster ${CLUSTER_NAME} Successfully deployed"
+
 
 # Just proving it works fine
 echo "DO YOU WANT TO DEPLOY A TEST APP (ECHOSERVER) TO PROVE IT'S WORKING?"
@@ -70,11 +86,11 @@ read question
 
 kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/aws-alb-ingress-controller/v1.1.0/docs/examples/echoservice/echoserver-namespace.yaml && \
 kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/aws-alb-ingress-controller/v1.1.0/docs/examples/echoservice/echoserver-service.yaml && \
-kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/aws-alb-ingress-controller/v1.1.0/docs/examples/echoservice/echoserver-deployment.yaml
-
-curl -s -O https://raw.githubusercontent.com/kubernetes-sigs/aws-alb-ingress-controller/v1.1.0/docs/examples/echoservice/echoserver-ingress.yaml
-sed -i '' 's/echoserver.example.com//g' echoserver-ingress.yaml
-kubectl apply -f echoserver-ingress.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/aws-alb-ingress-controller/v1.1.0/docs/examples/echoservice/echoserver-deployment.yaml && \
+curl -s -O https://raw.githubusercontent.com/kubernetes-sigs/aws-alb-ingress-controller/v1.1.0/docs/examples/echoservice/echoserver-ingress.yaml && \
+sed -i '' 's/echoserver.example.com//g' echoserver-ingress.yaml && \
+kubectl apply -f echoserver-ingress.yaml && \
+rm -f echoserver-ingress.yaml && \
 print_log $? "echoserver-ingress"
 sleep 15
 LB_ADDR=$(kubectl describe ing -n echoserver echoserver | grep 'Address:' | awk '{ print $NF }')
@@ -97,5 +113,4 @@ done
 
 echo
 echo
-
 print_log $? "echoserver up and running on http://${LB_ADDR}"
